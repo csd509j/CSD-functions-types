d5184ffa66f0be6626208b6ac33e18da9488583a
Josh Armentano <josh@abidewebdesign.com>
2018-08-25 12:02:48 -0700

    Change: Removed bootstrap tools from content editor

--- csd-functions-toolset-types.php
+++ csd-functions-toolset-types.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: CSD Functions - Toolset Types
-Version: 1.0
+Version: 1.1
 Description: Toolset Types Customizations for CSD Schools and District Theme
 Author: Josh Armentano
 Author URI: https://abidewebdesign.com
@@ -38,3 +38,19 @@ function add_my_post_types_to_query( $query ) {
   return $query;
 }
 add_action( 'pre_get_posts', 'add_my_post_types_to_query' );
+
+/*
+ * Remove bootstrap tools
+ *
+ * @since CSD Schools 1.4.9
+ */
+add_filter( 'mce_buttons_3', 'remove_bootstrap_buttons', 999 );
+function remove_bootstrap_buttons($buttons) {
+    return array();
+}
+   
+add_filter( 'mce_buttons', 'remove_toggle_button', 999 );
+function remove_toggle_button($buttons) {
+    $remove = array( 'css_components_toolbar_toggle' );
+    return array_diff( $buttons, $remove ); 
+} 
